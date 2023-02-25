9634f1b3cd7785f465d2521792e6c2c45d63dd36
qqqq4u <danila16102003@rambler.ru>
2023-02-25 20:13:07 +0300

    initial 4

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-	authDelivery "github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/auth/delivery/http"
-	authRepository "github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/auth/repo"
-	authUsecase "github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/auth/usecase"
 	"github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/middleware"
 	"github.com/gorilla/mux"
 	"log"
-	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -23,34 +19,35 @@ func run() error {
 	r := mux.NewRouter()
 
 	r.Use(middleware.CORSMiddleware)
-
-	srv := http.Server{Handler: r, Addr: fmt.Sprintf(":%s", "8000")}
-
-	conn, err := config.GetConnectionString() //вытаскивать зашитые данные придумать!!!
-	if err != nil {
-		return err
-	}
+	time.Sleep(20 * time.Minute)
+	//
+	//srv := http.Server{Handler: r, Addr: fmt.Sprintf(":%s", "8000")}
+	//
+	//conn, err := config.GetConnectionString() //вытаскивать зашитые данные придумать!!!
+	//if err != nil {
+	//	return err
+	//}
 
 	//pool, err := pgxpool.Connect(context.Background(), conn)
 	//if err != nil {
 	//	return err
 	//}
-
-	tokenGenerator := authUsecase.NewTokenator()
-	authRepo := authRepository.NewAuthRepo(pool)
-	authUse := authUsecase.NewAuthUsecase(authRepo, tokenGenerator)
-	authHandler := authDelivery.NewAuthHandler(authUse, onlineRepo)
-
-	auth := r.PathPrefix("/user").Subrouter()
-	{
-		auth.HandleFunc("/login", authHandler.Login).Methods(http.MethodPost)
-		auth.HandleFunc("/logout", authHandler.Logout).Methods(http.MethodPost, http.MethodOptions)
-		auth.HandleFunc("/signup", authHandler.SignUp).Methods(http.MethodPost)
-		auth.HandleFunc("/auth", authHandler.AuthStatus).Methods(http.MethodGet)
-	}
-
-	http.Handle("/", r)
-	log.Print("main running on: ", srv.Addr)
-	return srv.ListenAndServe()
+	//
+	//tokenGenerator := authUsecase.NewTokenator()
+	//authRepo := authRepository.NewAuthRepo(pool)
+	//authUse := authUsecase.NewAuthUsecase(authRepo, tokenGenerator)
+	//authHandler := authDelivery.NewAuthHandler(authUse, onlineRepo)
+	//
+	//auth := r.PathPrefix("/user").Subrouter()
+	//{
+	//	auth.HandleFunc("/login", authHandler.Login).Methods(http.MethodPost)
+	//	auth.HandleFunc("/logout", authHandler.Logout).Methods(http.MethodPost, http.MethodOptions)
+	//	auth.HandleFunc("/signup", authHandler.SignUp).Methods(http.MethodPost)
+	//	auth.HandleFunc("/auth", authHandler.AuthStatus).Methods(http.MethodGet)
+	//}
+	//
+	//http.Handle("/", r)
+	//log.Print("main running on: ", srv.Addr)
+	//return srv.ListenAndServe()
 	return nil
 }
