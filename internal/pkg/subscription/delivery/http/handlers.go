df8c95d1d1517ff657107ea673a57f0ee756cdf0
qqqq4u <danila16102003@rambler.ru>
2023-04-18 23:38:41 +0300

    sub edit

--- internal/pkg/subscription/delivery/http/handlers.go
+++ internal/pkg/subscription/delivery/http/handlers.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"errors"
+	"fmt"
 	"github.com/go-park-mail-ru/2023_1_4from5/internal/models"
 	"github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/auth"
 	"github.com/go-park-mail-ru/2023_1_4from5/internal/pkg/subscription"
@@ -142,6 +143,84 @@ func (h *SubscriptionHandler) DeleteSubscription(w http.ResponseWriter, r *http.
 		utils.Response(w, http.StatusForbidden, nil)
 		return
 	} else if err != nil {
+		utils.Response(w, http.StatusInternalServerError, nil)
+		return
+	}
+
+	utils.Response(w, http.StatusOK, nil)
+}
+
+func (h *SubscriptionHandler) EditSubscription(w http.ResponseWriter, r *http.Request) {
+
+	subscriptionIDTmp, ok := mux.Vars(r)["sub-uuid"]
+	if !ok {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+	subscriptionInfo := models.Subscription{}
+	tmp, err := uuid.Parse(subscriptionIDTmp)
+	if err != nil {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+	subscriptionInfo.Id = tmp
+	//почему я не могу писать сразу в айдишку?
+	userDataJWT, err := token.ExtractJWTTokenMetadata(r)
+	if err != nil {
+		utils.Response(w, http.StatusUnauthorized, nil)
+		return
+	}
+
+	if _, err := h.authUsecase.CheckUserVersion(r.Context(), *userDataJWT); err != nil {
+		utils.Cookie(w, "", "SSID")
+		utils.Response(w, http.StatusForbidden, nil)
+		return
+	}
+	if r.Method == http.MethodGet {
+		tokenCSRF, err := token.GetCSRFToken(models.User{Login: userDataJWT.Login, Id: userDataJWT.Id, UserVersion: userDataJWT.UserVersion})
+		if err != nil {
+			utils.Response(w, http.StatusUnauthorized, nil)
+			return
+		}
+		utils.ResponseWithCSRF(w, tokenCSRF)
+		return
+	}
+
+	userDataCSRF, err := token.ExtractCSRFTokenMetadata(r)
+	if err != nil || *userDataCSRF != *userDataJWT {
+		utils.Response(w, http.StatusForbidden, nil)
+		return
+	}
+
+	if err = easyjson.UnmarshalFromReader(r.Body, &subscriptionInfo); err != nil {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+	if !subscriptionInfo.IsValid() {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+	if subscriptionInfo.Creator == uuid.Nil {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+
+	creatorID, isCreator, err := h.userUsecase.CheckIfCreator(r.Context(), userDataJWT.Id)
+	if err != nil {
+		utils.Response(w, http.StatusInternalServerError, nil)
+		return
+	} else if !isCreator {
+		utils.Response(w, http.StatusBadRequest, nil)
+		return
+	}
+
+	if creatorID != subscriptionInfo.Creator {
+		utils.Response(w, http.StatusForbidden, nil)
+		return
+	}
+	fmt.Println(subscriptionInfo)
+
+	if err = h.usecase.EditSubscription(r.Context(), subscriptionInfo); err != nil {
 		utils.Response(w, http.StatusInternalServerError, nil)
 		return
 	}
