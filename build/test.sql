b094980f546617c6f3818cfd11763a156a563b48
DESOLATE17 <taktashovadasha@yandex.ru>
2023-04-26 23:09:14 +0300

    fixed GetPage

--- build/test.sql
+++ build/test.sql
@@ -13,3 +13,4 @@ CREATE TEXT SEARCH DICTIONARY russian_ispell (
     AffFile = russian,
     StopWords = russian
     );
+SELECT "post".post_id, creation_date, title, post_text, likes_count, array_agg(attachment_id), array_agg(attachment_type), array_agg(DISTINCT subscription_id) FROM "post" LEFT JOIN "attachment" a on "post".post_id = a.post_id LEFT JOIN "post_subscription" ps on "post".post_id = ps.post_id WHERE creator_id = '10b0d1b8-0e67-4e7e-9f08-124b3e32cce4' GROUP BY "post".post_id, creation_date, title, post_text ORDER BY creation_date DESC;
